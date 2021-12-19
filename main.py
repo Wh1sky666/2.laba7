6a65b33268feccc2444312902258b9df161037b6
Ростислав Александрович Боржонов <80468507+Wh1sky666@users.noreply.github.com>
2021-12-19 15:58:19 +0300

    zadanie 1

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import mymodule
+if __name__ == '__main__':
+    print(mymodule.first(list)('1 2 3 4 5 6 7 8 9'))
+    print(mymodule.first(tuple)('1 2 3 4 5 6 7 8 9'))
+
